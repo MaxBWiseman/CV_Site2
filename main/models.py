2a0ef366d26ad9e95013bc68653d806180e73845
Max.W <maxwise70@hotmail.co.uk>
2025-04-28 18:11:39 +0000

    task: included projects, skills and blogpost models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Projects(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    project_banner_image = models.ImageField(upload_to='project_banner_images/')
+    link = models.URLField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project_images = models.ImageField(upload_to='project_images/')
+
+    def __str__(self):
+        return self.title
+
+class Skills(models.Model):
+    skill_name = models.CharField(max_length=100)
+    skill_level = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.skill_name
+    
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    content = models.TextField()
+    blog_banner_image = models.ImageField(upload_to='blog_banner_images/')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog_images = models.ImageField(upload_to='blog_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
