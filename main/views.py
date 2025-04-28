b8b740f9dbc27ee217c78c91055fbce471225ba7
Max.W <maxwise70@hotmail.co.uk>
2025-04-28 19:42:41 +0000

    task: added environment variables for Google Maps API and secret key
    Updated views and templates to use these new .env variables

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from django.conf import settings
 from django.views import View
 
 # Create your views here.
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'main/index.html')
+        return render(request, 'main/index.html', {
+            'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
+        })
     
 class ResumeView(View):
     def get(self, request):
--- my_cv_site/settings.py
+++ my_cv_site/settings.py
@@ -27,7 +27,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$33_jd_p-u$!eie+zrxs6psj$*wfd53^rejhn)%5%yr=*s6w67"
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
