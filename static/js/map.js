43b305ac950b707d1ea00b5495bd0efe23279c95
Max.W <maxwise70@hotmail.co.uk>
2025-05-20 14:34:35 +0000

    chore: updated photo of me and map.js locations

--- static/js/map.js
+++ static/js/map.js
@@ -24,14 +24,8 @@ async function initMap() {
                     "</div>" +
                     '<h2 id="firstHeading" class="firstHeading">My Home, Devon</h2>' +
                     '<div id="bodyContent">' +
-                    "<p><b>About:</b> Being a software developer in <b>Devon, UK,</b> offers a unique blend of " +
-                    "professional and lifestyle benefits. The region is home to a growing tech " +
-                    "community, with opportunities to work on innovative projects across various " +
-                    "industries. Companies in Exeter, Plymouth, and other parts of Devon are " +
-                    "increasingly seeking skilled developers, providing a fertile ground for career " +
-                    "growth. The picturesque landscapes and coastal beauty of Devon also " +
-                    "offer a serene backdrop for work-life balance, making it an ideal location for " +
-                    "those who appreciate both professional challenges and a quiet quality of life. " +
+                    "<p><b>About:</b> Being a software developer in <b>Devon, UK,</b> offers a great work-life balance" +
+                    ", though I am keen to explore as there is more to life than my home. Expanding my horizons will broaden my oppertunities" +
                     "</div>" +
                     "</div>",
             },
